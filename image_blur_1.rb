15c02920f2098353182460d8c0d15ee7e79e66e4
Esteban Espinoza <estebanespinoza146@gmail.com>
2019-09-18 03:04:38 +0000

    finished image blur 1

--- image_blur_1.rb
+++ image_blur_1.rb
@@ -1,3 +1,15 @@
+class  Image
+  def initialize(array)
+    @arra = array
+  end
+
+  def output_image
+    @arra.each do |x|
+      puts x.join("")
+    end
+  end
+end
+
 image = Image.new([
   [0, 0, 0, 0],
   [0, 1, 0, 0],
@@ -5,3 +17,4 @@ image = Image.new([
   [0, 0, 0, 0]
 ])
 image.output_image
+
